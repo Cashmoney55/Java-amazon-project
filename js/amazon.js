a237d50dd3d9fe48b2c2edb35ffd0fff1edc853d
jules <teddyjules02@gmail.com>
2026-01-05 16:55:28 +0000

    save data and generate html for delivery  options

--- js/amazon.js
+++ js/amazon.js
@@ -1,6 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import { product } from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import formatCurrency  from './utils/money.js';
 
 
 
